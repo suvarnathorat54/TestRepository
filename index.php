6f31586b47c70747611a293ed523ef5d9802df88
suvarnathorat54 <146089094+suvarnathorat54@users.noreply.github.com>
2023-09-27 10:35:33 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,59 @@
+function apiRequest($url, $post=FALSE, $headers=array()) {
+  $ch = curl_init($url);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+ 
+  if($post)
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post));
+ 
+  $headers = [
+    'Accept: application/vnd.github.v3+json, application/json',
+    'User-Agent: https://example-app.com/'
+  ];
+ 
+  if(isset($_SESSION['access_token']))
+    $headers[] = 'Authorization: Bearer '.$_SESSION['access_token'];
+ 
+  curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+ 
+  $response = curl_exec($ch);
+  return json_decode($response, true);
+}
+
+
+// Fill these out with the values from GitHub
+$githubClientID = 'ecaba879953c29a9b812';
+$githubClientSecret = 'e9fbce9e4abd16fc46911ec59e097f75a959c115';
+ 
+// This is the URL we'll send the user to first
+// to get their authorization
+$authorizeURL = 'https://github.com/login/oauth/authorize';
+ 
+// This is the endpoint we'll request an access token from
+$tokenURL = 'https://github.com/login/oauth/access_token';
+ 
+// This is the GitHub base URL for API requests
+$apiURLBase = 'https://api.github.com/';
+ 
+// The URL for this script, used as the redirect URL
+$baseURL = 'https://' . $_SERVER['SERVER_NAME']
+    . $_SERVER['PHP_SELF'];
+ 
+// Start a session so we have a place to
+// store things between redirects
+session_start();
+
+
+
+// If there is an access token in the session
+// the user is already logged in
+if(!isset($_GET['action'])) {
+  if(!empty($_SESSION['access_token'])) {
+    echo '<h3>Logged In</h3>';
+    echo '<p><a href="?action=repos">View Repos</a></p>';
+    echo '<p><a href="?action=logout">Log Out</a></p>';
+  } else {
+    echo '<h3>Not logged in</h3>';
+    echo '<p><a href="?action=login">Log In</a></p>';
+  }
+  die();
+}
